aa299cc22194a9b26934b029d4997a5a94a0bfdf
Ido-c <its.ido@gmail.com>
2020-06-10 17:20:00 +0300

    parityCheck in AQPHashTable

--- src/AQPHashTable.java
+++ src/AQPHashTable.java
@@ -9,7 +9,11 @@ public class AQPHashTable extends OAHashTable {
 	}
 	@Override
 	public int Hash(long x, int i) {
-		int res=(modHash.Hash(x)+(int)(Math.pow(-1,i))*i*i);
+		int res=(modHash.Hash(x)+(parityCheck(i)*i*i));
 		return (m+(res%m))%m;
+	}
+	private int parityCheck(int i) {
+		if (i % 2 == 0) return 1;
+		return -1;
 	}
 }
