c622b8daf8ac87b5b2ff8a8256d4b4657fc9a897
Ido-c <its.ido@gmail.com>
2020-06-11 12:19:44 +0300

    finished Documentation

--- src/LPHashTable.java
+++ src/LPHashTable.java
@@ -1,5 +1,6 @@
-import java.util.Random;
-
+/**
+ * HashTable using open addressing with linear probing
+ */
 public class LPHashTable extends OAHashTable {
 	private ModHash modHash;
 	public LPHashTable(int m, long p) {
@@ -7,7 +8,14 @@ public class LPHashTable extends OAHashTable {
 		modHash = ModHash.GetFunc(m,p);
 
 	}
-	
+
+	/**
+	 *
+	 * @param x - the key to hash
+	 * @param i - the index in the probing sequence
+	 * @return product of Hash function (h(x)+i)%m
+	 * h is a function from a universal family
+	 */
 	@Override
 	public int Hash(long x, int i) {
 		return (modHash.Hash(x)+i)%m;
